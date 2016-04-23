bb66b60cd8d71f6fde20de78a45a691af7735243
Nikolay Zamulov <kolia8@mail.ru>
2016-04-23 21:41:39 +0300

    Move info about sections into config.js file.

--- www/js/config.js
+++ www/js/config.js
@@ -1,3 +1,5 @@
 module.exports = {
-	'APIkey' : 'b5171c0f6676d74da6cb99f7ccfc6aaa:0:75059933'
+	'APIkey' : 'b5171c0f6676d74da6cb99f7ccfc6aaa:0:75059933',
+	'sections' : ['Home', 'World', 'National', 'Politics', 'Nyregion', 'Business', 'Opinion', 'Technology',
+				  'Science', 'Health', 'Sports', 'Arts', 'Fashion', 'Dining', 'Travel', 'Magazine', 'Realestate']
 }
--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const config = require('./config.js');
 const Connector = require('./connector.js');
 
 class Controller {
@@ -7,11 +8,23 @@ class Controller {
 	constructor() {
 		
 		let sectionsButton = document.getElementById('sections-button');
-		let searchButton = document.getElementById('search-button');
 		let githubButton = document.getElementById('github-button');
 
 		sectionsButton.addEventListener('click', this.sectionsButtonClickHandler, false);
 		githubButton.addEventListener('click', this.githubButtonClickHandler, false);
+
+		let sectionsMenu = document.getElementById('sections-menu-items');
+
+		for(let section of config.sections) {
+
+			let li = document.createElement('li');
+
+			li.innerHTML = section;
+
+			li.addEventListener('click', this.sectionsMenuItemClickHandler, false);
+
+			sectionsMenu.appendChild(li);
+		}
 
 	}
 
@@ -24,10 +37,18 @@ class Controller {
 		} else {
 			seactionsMenu.style.display = "block";
 		}
+
 	}
 
 	githubButtonClickHandler() {
 		location.href = "https://github.com/ZamNick/epam.test.project";
+	}
+
+
+	sectionsMenuItemClickHandler() {
+		
+		
+		
 	}
 
 }
