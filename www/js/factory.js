b6631d23d69da831c4fd68d30d5b04d55bc83893
Nikolay Zamulov <kolia8@mail.ru>
2016-04-26 22:47:07 +0300

    Create documentation for factory.

--- www/js/factory.js
+++ www/js/factory.js
@@ -2,8 +2,30 @@
 
 import Note from './note.js';
 
+/**
+ * A class represent an instance that create needed
+ * news note using passed metadata of current one news
+ * and needed type of note.
+ * @class
+ *
+ * @requires note.js
+ * @author Nikolay Zamulov <zamulov8@gmail.com> 
+ */
 class Factory {
 
+
+
+	/**
+	 * Function create a needed type of current one news note.
+	 * Represent a simple factory pattern.
+	 * 
+	 * @function
+	 * @param {Object} data - Metadata of current one news.
+	 * @param {String} type - Needed type for current one news note.
+	 * @static
+	 * @returns {String | Error} - HTML code of current one news for adding it into 
+	 * web-site content or error for unknow type note.
+	 */
 	static createNote(data, type) {
 		if("main" === type) {
 			return Note.getMainNote(data);
